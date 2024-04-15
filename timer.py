824cbb00906a2c5a955ae5d13f2a13fff353324c
filipploch <filip.ploch@gmail.com>
2024-04-15 10:29:23 +0200

    timer.py refactor

--- timer.py
+++ timer.py
@@ -2,14 +2,13 @@ import time
 import datetime
 from models import Match
 from database import db
-from variables import MATCH_ID
 from flask import current_app
 
 
 class Timer:
     def __init__(self, app):
         with app.app_context():
-            self.actual_match = Match.query.filter_by(id=MATCH_ID).first()
+            self.actual_match = Match.query.filter_by(actual=1).first()
             self.start_time = time.time()
             self.end_time = time.time()
 
@@ -17,9 +16,9 @@ class Timer:
         with app.app_context():
             # self.start_time = time.time()
             while True:
-                if Match.query.filter_by(id=MATCH_ID).first().is_timer_active == 1:
+                if Match.query.filter_by(actual=1).first().is_timer_active == 1:
                     self.timer_add_seconds(seconds=1)
-                elif Match.query.filter_by(id=MATCH_ID).first().is_timer_active == 2:
+                elif Match.query.filter_by(actual=1).first().is_timer_active == 2:
                     self.timer_add_seconds(seconds=0)
                     self.timer_reset()
                 else:
@@ -27,11 +26,11 @@ class Timer:
                 time.sleep(1)
 
     def timer_add_seconds(self, seconds: int):
-        match_time = Match.query.filter_by(id=MATCH_ID).first().seconds
+        match_time = Match.query.filter_by(actual=1).first().seconds
         match_time += seconds
-        Match.query.filter_by(id=MATCH_ID).first().seconds = match_time
+        Match.query.filter_by(actual=1).first().seconds = match_time
         db.session.commit()
 
     def timer_reset(self):
-        Match.query.filter_by(id=MATCH_ID).first().seconds = 0
+        Match.query.filter_by(actual=1).first().seconds = 0
         db.session.commit()
